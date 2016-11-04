9e8b93dafc4a078a7a8fd219c23b1ea96ae1be34
devjem <android.devjem@gmail.com>
2016-11-04 00:26:14 -0500

    First commit

---
+++ TicketMaster.h
@@ -0,0 +1,36 @@
+
+#ifndef TICKETMASTER_H
+#define TICKETMASTER_H
+#include <iostream>
+using namespace std;
+
+class TicketMaster {
+public:
+
+	int displaySeats() {
+		cout << "displaySeats\n";
+		return 0;
+	}
+
+	int requestTicekts() {
+		cout << "requestTicekts\n";
+		return 0;
+	}
+		
+	int purchaseTickets() {
+		cout << "purchaseTickets\n";
+		return 0;
+	}
+
+	int salesReport() {
+		cout << "salesReport\n";
+		return 0;
+	}
+
+};
+
+// TicketMaster::TicketMaster () {
+// 	// read data from files
+// 	// set up array
+// }
+#endif
---
+++ project.cpp
@@ -0,0 +1,39 @@
+
+
+#include <iostream>
+#include "TicketMaster.h"
+
+using namespace std;
+
+
+int main() {
+
+	int choice = 0;
+	TicketMaster tm;
+
+	do {
+		while (true) {
+			cout << "Please select an option:\n1: Display Seating Chart\n2: Request Tickets\n3: Print Sales Report\n4: Exit\n   ";
+			cin >> choice;
+
+			if (choice > 0 && choice < 5)
+				break;
+			else 
+				cout << "Invalid entry. Please try again.\n";
+		}
+
+	
+		switch (choice) {
+			case 1: tm.displaySeats();
+							break;
+			case 2: tm.requestTicekts();
+							break;
+			case 3: tm.salesReport();
+							break;
+			case 4: cout << "Goodbye!\n";
+							return 0;
+		}
+	} while (choice != 4);
+
+	return 0;
+}
